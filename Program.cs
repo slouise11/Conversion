1fbe21a99cddcad15184f9a2323e26c1d62962c3
Sarah <Sarah@DESKTOP-M75431O>
2024-10-13 16:43:24 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Conversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Conversion";
+
+            Console.WriteLine("Please Enter A Number: ");
+            double num = Convert.ToDouble( Console.ReadLine() );
+            Console.WriteLine("Now Enter Another Number: ");
+            double sum =
+                num + Convert.ToDouble(Console.ReadLine() );
+
+            Console.WriteLine("Total = " + sum );
+            Console.ReadKey();
+        }
+    }
+}
